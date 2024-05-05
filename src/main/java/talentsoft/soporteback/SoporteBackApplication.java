a4968c12e931f548561efa03a6ab8f0f27381e37
Sebastian Orjuela <sebasorjuela@gmail.com>
2024-05-05 13:05:11 -0500

    First commit

---
+++ src/main/java/talentsoft/soporteback/SoporteBackApplication.java
@@ -0,0 +1,13 @@
+package talentsoft.soporteback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoporteBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoporteBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/talentsoft/soporteback/SoporteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package talentsoft.soporteback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoporteBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
